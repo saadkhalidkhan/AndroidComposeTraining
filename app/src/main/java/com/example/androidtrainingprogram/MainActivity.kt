3c02c61d50da87b70377d8f4f7f97b8fa13a1048
m.saadkhan <m.saadkhan@systemstd.com>
2023-09-15 20:02:09 +0500

    my cv created

--- app/src/main/java/com/example/androidtrainingprogram/MainActivity.kt
+++ app/src/main/java/com/example/androidtrainingprogram/MainActivity.kt
@@ -1,41 +1,31 @@
 package com.example.androidtrainingprogram
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.SnackbarHost
-import androidx.compose.material.SnackbarHostState
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidtrainingprogram.ui.theme.AndroidTrainingProgramTheme
-import com.example.androidtrainingprogram.ui.theme.Purple40
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -43,124 +33,375 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidTrainingProgramTheme {
-                MainScreen()
+                MyCVScreen()
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun MainScreen() {
-    val snackbarHostState = remember {
-        SnackbarHostState()
-    }
-
-    val scope = rememberCoroutineScope()
+fun MyCVScreen() {
 
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        "Android Training Program",
-                        style = MaterialTheme.typography.body1
-                    )
-                },
-                actions = {
-                    IconButton(onClick = { /* doSomething() */ }) {
-                        Icon(
-                            Icons.Filled.Favorite,
-                            contentDescription = "Localized description"
-                        )
-                    }
-
-                    IconButton(onClick = {
-                        scope.launch {
-                            snackbarHostState.showSnackbar("This is Dummy Snackbar!")
-                        }
-                    }) {
-                        Icon(
-                            Icons.Filled.Settings,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                }
-            )
-        },
-        snackbarHost = {
-            SnackbarHost(hostState = snackbarHostState)
-        }
     ) {
         val state = rememberScrollState()
 
         Column(
             modifier = Modifier
-                .padding(it)
+                .padding(8.dp)
+                .fillMaxWidth()
                 .verticalScroll(state)
         ) {
-            /*Text(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .background(Color.Black)
-                    .padding(8.dp),
-                text = stringResource(id = R.string.dummy_text),
-                style = MaterialTheme.typography.body1.copy(
-                    color = Color.White
-                )
-            )*/
 
-            Text(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .background(Color.Black, shape = RoundedCornerShape(8.dp))
-                    .border(color = Color.Red, width = 1.dp, shape = RoundedCornerShape(8.dp))
-                    .padding(8.dp)
-                    .clickable {
-                        scope.launch {
-                            snackbarHostState.showSnackbar("This is Dummy Snackbar!")
-                        }
-                    },
-                text = "Welcome",
-                style = MaterialTheme.typography.body1.copy(
-                    color = Color.White
-                )
-            )
+            //-: user name and designation
+            UserInfo()
+
+            //-: user professional summary
+            UserProfessionalSummary()
+
+            //-: user experiences
+            UserExperiences()
+
+            //-: user skills
+            UserSkills()
+
+            //-: user languages
+            UserLanguages()
+
+            //-: user hobbies
+            UserHobbies()
         }
     }
 }
 
 
 @Composable
-fun WelcomeUser(name: String) {
-    Box(
-        modifier = Modifier.background(Purple40)
+fun UserInfo() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
     ) {
-        Text(
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth()
-                .background(Color.Black)
-                .padding(8.dp),
-            text = "Hello $name!",
-            textDecoration = TextDecoration.LineThrough,
-            style = MaterialTheme.typography.body1.copy(
-                color = Color.White
-            )
+
+        TextView(
+            text = stringResource(id = R.string.user_name),
+            modifier = Modifier.padding(vertical = 8.dp),
+            fontSize = 24.sp,
+            textColor = Color.Black,
+            textStyle = MaterialTheme.typography.h1,
+            fontWeight = FontWeight.Bold
         )
+
+        TextView(
+            text = stringResource(id = R.string.user_designation),
+            fontSize = 16.sp,
+            textColor = Color.Black,
+            textStyle = MaterialTheme.typography.body1,
+            fontWeight = FontWeight.Normal
+        )
+
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun WelcomeUserPreview() {
-    WelcomeUser(name = "Welcome Home")
+fun UserProfessionalSummary() {
+    TextView(
+        text = stringResource(id = R.string.user_professional_summary_title),
+        modifier = Modifier.padding(vertical = 8.dp),
+        fontSize = 24.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.h1,
+        fontWeight = FontWeight.Bold,
+        textDecoration = TextDecoration.Underline
+    )
+
+    TextView(
+        text = stringResource(id = R.string.user_professional_summary),
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
 }
 
+@Composable
+fun UserExperiences() {
+    TextView(
+        text = stringResource(id = R.string.user_experience_title),
+        modifier = Modifier.padding(vertical = 8.dp),
+        fontSize = 24.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.h1,
+        fontWeight = FontWeight.Bold,
+        textDecoration = TextDecoration.Underline
+    )
+
+    TextView(
+        text = stringResource(id = R.string.user_experience_company),
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+    TextView(
+        text = stringResource(id = R.string.user_experience_company_duration),
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start,
+        fontStyle = FontStyle.Italic
+    )
+
+    Divider(thickness = 1.dp, color = Color.LightGray, modifier = Modifier.padding(vertical = 4.dp))
+
+    TextView(
+        text = stringResource(id = R.string.user_experience_company),
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+    TextView(
+        text = stringResource(id = R.string.user_experience_company_duration),
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start,
+        fontStyle = FontStyle.Italic
+    )
+
+    Divider(thickness = 1.dp, color = Color.LightGray, modifier = Modifier.padding(vertical = 4.dp))
+
+    TextView(
+        text = stringResource(id = R.string.user_experience_company),
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+    TextView(
+        text = stringResource(id = R.string.user_experience_company_duration),
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start,
+        fontStyle = FontStyle.Italic
+    )
+
+    Divider(thickness = 1.dp, color = Color.LightGray, modifier = Modifier.padding(vertical = 4.dp))
+
+    TextView(
+        text = stringResource(id = R.string.user_experience_company),
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+    TextView(
+        text = stringResource(id = R.string.user_experience_company_duration),
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start,
+        fontStyle = FontStyle.Italic
+    )
+
+    Divider(thickness = 1.dp, color = Color.LightGray, modifier = Modifier.padding(vertical = 4.dp))
+
+    TextView(
+        text = stringResource(id = R.string.user_experience_company),
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+    TextView(
+        text = stringResource(id = R.string.user_experience_company_duration),
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start,
+        fontStyle = FontStyle.Italic
+    )
+
+    Divider(thickness = 1.dp, color = Color.LightGray, modifier = Modifier.padding(vertical = 4.dp))
+}
+
+@Composable
+fun UserSkills() {
+    TextView(
+        text = stringResource(id = R.string.user_skill),
+        modifier = Modifier.padding(vertical = 8.dp),
+        fontSize = 24.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.h1,
+        fontWeight = FontWeight.Bold,
+        textDecoration = TextDecoration.Underline
+    )
+
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_skill_android)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_skill_kotlin)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_skill_java)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_skill_compose)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_skill_mvvm)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_skill_retrofit)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_skill_project_management)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+}
+
+
+@Composable
+fun UserLanguages() {
+    TextView(
+        text = stringResource(id = R.string.user_languages),
+        modifier = Modifier.padding(vertical = 8.dp),
+        fontSize = 24.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.h1,
+        fontWeight = FontWeight.Bold,
+        textDecoration = TextDecoration.Underline
+    )
+
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_language_english)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_language_urdu)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_language_punjabi)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+}
+
+@Composable
+fun UserHobbies() {
+    TextView(
+        text = stringResource(id = R.string.user_hobbies),
+        modifier = Modifier.padding(vertical = 8.dp),
+        fontSize = 24.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.h1,
+        fontWeight = FontWeight.Bold,
+        textDecoration = TextDecoration.Underline
+    )
+
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_hobbies_travelling)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_hobbies_video_games)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+    TextView(
+        text = "\u2022 ${stringResource(id = R.string.user_hobbies_cooking)}",
+        fontSize = 16.sp,
+        textColor = Color.Black,
+        textStyle = MaterialTheme.typography.body1,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Start
+    )
+
+}
 
 @Preview(showBackground = true)
 @Composable
 fun MainScreenPreview() {
     AndroidTrainingProgramTheme {
-        MainScreen()
+        MyCVScreen()
     }
 }
